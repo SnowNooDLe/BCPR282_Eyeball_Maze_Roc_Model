063a6241d819ec32262a2ae05bed3905264a7f79
SnowNooDLe <sdw123123@gmail.com>
2019-06-24 01:11:39 +1200

    need to fix go back move

--- app/src/main/java/dos0311/ara/ac/nz/eyeballmaze_rocs_model/MainActivity.java
+++ app/src/main/java/dos0311/ara/ac/nz/eyeballmaze_rocs_model/MainActivity.java
@@ -37,13 +37,19 @@ public class MainActivity extends AppCompatActivity {
     private int errorCount = 0;
     private int currRow;
     private int currCol;
-    private Piece theEndPiece, thePiece;
+    private int currentStage;
+    private int targetRow;
+    private int targetCol;
+    private Piece theEndPiece, thePiece, theStartPiece;
     private Eyeball eb;
     private GameGridIron gr;
 
-//    My previous
+//    Roc's model does not contain number of goal variable neither set/getter, allocating manually
+    private int goal;
 
-    private int size = 6;
+//    My previous
+    private int rowSize = 6;
+    private int colSize = 4;
 
 
     //    for loading & saving game
@@ -82,105 +88,161 @@ public class MainActivity extends AppCompatActivity {
         textViewForGoal = findViewById(R.id.textViewGoals);
         textViewForMovements = findViewById(R.id.textViewMovements);
 
+
+        startGame();
+    }
+    //  Extra View Feature 3, user can select level by clicking this.
+    public void stageOneSetup(){
+//        when start new game, always reset the number of goal
+        goal = 0;
+        for (int i = 0; i < rowSize; i++){
+            for (int j = 0; j < colSize; j++){
+                String imageview = "imageView" + i + j;
+                int resID = getResources().getIdentifier(imageview, "id", getPackageName());
+                imageViews[i][j] = findViewById(resID);
+                imageSrcs[i][j] = R.drawable.empty_block;
+            }
+        }
         //      first row
-        imageViews[0][0] = findViewById(R.id.imageView00);
-        imageViews[0][1] = findViewById(R.id.imageView01);
-        imageViews[0][2] = findViewById(R.id.imageView02);
-        imageViews[0][3] = findViewById(R.id.imageView03);
-
-
-        imageSrcs[0][0] = R.drawable.empty_block;
-        imageSrcs[0][1] = R.drawable.empty_block;
         imageSrcs[0][2] = R.drawable.red_flower;
-        imageSrcs[0][3] = R.drawable.empty_block;
 
-        //      second row
-        imageViews[1][0] = findViewById(R.id.imageView10);
-        imageViews[1][1] = findViewById(R.id.imageView11);
-        imageViews[1][2] = findViewById(R.id.imageView12);
-        imageViews[1][3] = findViewById(R.id.imageView13);
-
-
+        //        second row
         imageSrcs[1][0] = R.drawable.cyan_cross;
         imageSrcs[1][1] = R.drawable.yellow_flower;
         imageSrcs[1][2] = R.drawable.yellow_diamond;
         imageSrcs[1][3] = R.drawable.green_cross;
 
-        //      third row
-        imageViews[2][0] = findViewById(R.id.imageView20);
-        imageViews[2][1] = findViewById(R.id.imageView21);
-        imageViews[2][2] = findViewById(R.id.imageView22);
-        imageViews[2][3] = findViewById(R.id.imageView23);
-
-
+        //      Third row
         imageSrcs[2][0] = R.drawable.green_flower;
         imageSrcs[2][1] = R.drawable.red_star;
         imageSrcs[2][2] = R.drawable.green_star;
         imageSrcs[2][3] = R.drawable.yellow_diamond;
 
         //      fourth row
-        imageViews[3][0] = findViewById(R.id.imageView30);
-        imageViews[3][1] = findViewById(R.id.imageView31);
-        imageViews[3][2] = findViewById(R.id.imageView32);
-        imageViews[3][3] = findViewById(R.id.imageView33);
-
-
         imageSrcs[3][0] = R.drawable.red_flower;
         imageSrcs[3][1] = R.drawable.cyan_flower;
         imageSrcs[3][2] = R.drawable.red_star;
         imageSrcs[3][3] = R.drawable.green_flower;
 
-
-        //      fifth row
-        imageViews[4][0] = findViewById(R.id.imageView40);
-        imageViews[4][1] = findViewById(R.id.imageView41);
-        imageViews[4][2] = findViewById(R.id.imageView42);
-        imageViews[4][3] = findViewById(R.id.imageView43);
-
-
+        //        Fifth row
         imageSrcs[4][0] = R.drawable.cyan_star;
         imageSrcs[4][1] = R.drawable.red_diamond;
         imageSrcs[4][2] = R.drawable.cyan_flower;
         imageSrcs[4][3] = R.drawable.cyan_diamond;
 
-        //      sixth row
-        imageViews[5][0] = findViewById(R.id.imageView50);
-        imageViews[5][1] = findViewById(R.id.imageView51);
-        imageViews[5][2] = findViewById(R.id.imageView52);
-        imageViews[5][3] = findViewById(R.id.imageView53);
-
-        imageSrcs[5][0] = R.drawable.empty_block;
+        //        Sixth row
         imageSrcs[5][1] = R.drawable.cyan_diamond;
-        imageSrcs[5][2] = R.drawable.empty_block;
-        imageSrcs[5][3] = R.drawable.empty_block;
 
+//      resetting the displayed image to new stage
+        for (int i = 0; i < rowSize; i++){
+            for (int j = 0; j < colSize; j++){
+                imageViews[i][j].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[i][j]));
+            }
+        }
+        //        for image
+        setGoalInMaze(0,2);
+        setPlayerInMaze(5,1);
+        goal++;
 
-        startGame();
+        gr = new GameGridIron(0);
+        eb = new Eyeball (gr);
+    }
+
+    public void stageTwoSetup(){
+//        when start new game, always reset the number of goal
+        goal = 0;
+        for (int i = 0; i < rowSize; i++){
+            for (int j = 0; j < colSize; j++){
+                String imageview = "imageView" + i + j;
+                int resID = getResources().getIdentifier(imageview, "id", getPackageName());
+                imageViews[i][j] = findViewById(resID);
+                imageSrcs[i][j] = R.drawable.empty_block;
+            }
+        }
+
+//      allocate images into imageSrces
+        imageSrcs[0][2] = R.drawable.red_flower;
+
+        imageSrcs[1][0] = R.drawable.cyan_cross;
+        imageSrcs[1][1] = R.drawable.cyan_flower;
+        imageSrcs[1][2] = R.drawable.cyan_diamond;
+        imageSrcs[1][3] = R.drawable.green_cross;
+
+        imageSrcs[2][0] = R.drawable.green_flower;
+        imageSrcs[2][1] = R.drawable.red_star;
+        imageSrcs[2][2] = R.drawable.green_star;
+        imageSrcs[2][3] = R.drawable.yellow_flower;
+
+        imageSrcs[3][0] = R.drawable.red_flower;
+        imageSrcs[3][1] = R.drawable.green_diamond;
+        imageSrcs[3][2] = R.drawable.red_star;
+        imageSrcs[3][3] = R.drawable.yellow_star;
+
+        imageSrcs[4][0] = R.drawable.green_cross;
+        imageSrcs[4][1] = R.drawable.red_diamond;
+        imageSrcs[4][2] = R.drawable.cyan_flower;
+        imageSrcs[4][3] = R.drawable.green_diamond;
+
+        imageSrcs[5][1] = R.drawable.cyan_diamond;
+
+//        resetting the displayed image to new stage
+        for (int i = 0; i < rowSize; i++){
+            for (int j = 0; j < colSize; j++){
+                imageViews[i][j].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[i][j]));
+            }
+        }
+
+//      for image
+        setGoalInMaze(0,2);
+        setPlayerInMaze(5,1);
+        goal++;
+
+        gr = new GameGridIron(1);
+        eb = new Eyeball (gr);
+    }
+
+    //    Starting the game with stage 1 but when it starts, always run stage one first
+    public void startGameStageOne() {
+        gameIsOn = true;
+        currentStage = 1;
+        stageOneSetup();
+        setTextForGoalMovement();
+    }
+    //    Starting the game with stage 2
+    public void startGameStageTwo(){
+        gameIsOn = true;
+        currentStage = 2;
+        stageTwoSetup();
+        setTextForGoalMovement();
+    }
+
+    //    Extra View Feature 3, choose stages via button click
+    public void startGameStageOneViaButton(View view){
+        startGameStageOne();
+    }
+
+    public void startGameStageTwoViaButton(View view){
+        startGameStageTwo();
     }
 
     //    Starting the game and always start with Stage 1.
     public void startGame() {
 //        Controller for Roc's Code
         gameIsOn = true;
-        gr = new GameGridIron(0);
-        eb = new Eyeball (gr);
 
+        stageOneSetup();
+
+//        Goal point
         theEndPiece = gr.findEndPointPiece();
 
-//        Plan was setting Red Flower for goal but for some reason,
-//        if eyeball goes to the REd Flower at Row : 0, Col : 3
-//        App creashes.
-//        board.setGoal(2, 4);
-//        eyeball = new Player(5,2, board);
-//
-//      Task 12. Display the number of goals to do
-//        textViewForGoal.setText(R.string.number_of_goals + "1");
-//       Task 13. Display move counts
-//        textViewForMovements.setText(R.string.number_of_movements + eb.countTotalMove());
+        setTextForGoalMovement();
 
-//        for image
-        setGoalInMaze(0,2);
-        setPlayerInMaze(5,1);
+    }
+    public void setTextForGoalMovement(){
+        //      Task 12. Display the number of goals to do
+        textViewForGoal.setText("Number of Goal(s) : " + goal);
+//       Task 13. Display move counts
+        textViewForMovements.setText("Number of Movements : " + eb.countTotalMove());
     }
 
     //    Task 5, Display image of player character
@@ -221,8 +283,8 @@ public class MainActivity extends AppCompatActivity {
 
             String targetPosition = getLocationImageView(nextImageView);
 //        to get row and col values
-            int targetRow = Character.digit(targetPosition.charAt(0), 10);
-            int targetCol = Character.digit(targetPosition.charAt(1), 10);
+            targetRow = Character.digit(targetPosition.charAt(0), 10);
+            targetCol = Character.digit(targetPosition.charAt(1), 10);
 
             thePiece = eb.getMyCurrentPiece();
 
@@ -241,7 +303,7 @@ public class MainActivity extends AppCompatActivity {
                 //        Debugging
                 Log.d("MYINT", "Resetting Target Row: " + targetRow);
                 Log.d("MYINT", "Resetting Target Col: " + targetCol);
-                imageViews[targetRow][targetCol].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[targetRow][targetCol]));
+//                imageViews[targetRow][targetCol].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[targetRow][targetCol]));
                 eb.moveToNextPieceSucceed(targetRow, targetCol);
 
                 movementHappening(targetRow, targetCol);
@@ -253,7 +315,7 @@ public class MainActivity extends AppCompatActivity {
 
 
 //        updating movements display
-//                textViewForMovements.setText(R.string.number_of_movements + eb.countTotalMove());
+                textViewForMovements.setText("Number of Movements : " + eb.countTotalMove());
             } else {
 //                just telling what to do
                 if (errorCount < 3){
@@ -267,11 +329,12 @@ public class MainActivity extends AppCompatActivity {
 //            debugging purpose
             Log.d("MYINT", "what is the EndPoint X: " + theEndPiece.x);
             Log.d("MYINT", "what is the EndPoint Y: " + theEndPiece.y);
+//            theEndPiece is the goal.
             if (targetRow == theEndPiece.x && targetCol == theEndPiece.y){
 //               Task 15, playing winning song
                 won_case_sound = MediaPlayer.create(MainActivity.this,R.raw.won_sound);
                 won_case_sound.start();
-//                textViewForGoal.setText(R.string.number_of_goals + "0");
+//                textViewForGoal.setText("Number of Goal(s) : " + "0");
                 gameFinishedMSG("Congratulations ! You won the game !");
                 gameIsOn = false;
             }
@@ -284,7 +347,7 @@ public class MainActivity extends AppCompatActivity {
                 gameIsOverBadEnding();
             }
         } else {
-            warningMSG("Game is finished, please restart the game if you want to");
+            warningMSG("Game is finished, please restart the game if you want");
         }
     }
 
@@ -301,13 +364,21 @@ public class MainActivity extends AppCompatActivity {
     private void resetStage(){
 //        resetting previous eyeball image
         gameIsOn = true;
-        imageViews[currRow][currCol].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[currRow][currCol]));
-        gr = new GameGridIron(0);
+        goal = 1;
+        imageViews[targetRow][targetCol].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[targetRow][targetCol]));
+        if (currentStage == 1){
+            gr = new GameGridIron(0);
+        } else if (currentStage == 2){
+            gr = new GameGridIron(1);
+        }
+
         eb = new Eyeball (gr);
         theEndPiece = gr.findEndPointPiece();
         setPlayerInMaze(5, 1);
         setGoalInMaze(0,2);
-        textViewForMovements.setText("Number of Movements: " + eb.countTotalMove());
+
+        textViewForGoal.setText("Number of Goal(s) : " + goal);
+        textViewForMovements.setText("Number of Movements : " + eb.countTotalMove());
     }
 
     //    Task 18 & Task 20,  Display dialogue with options after player character has lost
@@ -354,34 +425,25 @@ public class MainActivity extends AppCompatActivity {
     //    Extra view feature 1, go back one movement
     public void goBackOneMove(View view){
         if (checkGameIsOver()){
+//            To find starting point
+            theStartPiece = gr.findStartPointPiece();
             int movementsNum = eb.getMyAllMovedPieces().size();
             //        resetting current image without user.
             int currentRow = eb.myAllMovedPieces.get(movementsNum-1).x;
             int currentCol = eb.myAllMovedPieces.get(movementsNum-1).y;
-            imageViews[currentRow][currentCol].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[currentRow][currentCol]));
-//        go back one movement which will decrease number of movement, position as well
+//            compare whether player is at starting point
+            if (currentRow == theStartPiece.x && currentCol == theStartPiece.y){
+                warningMSG("You are at starting point, cannot go back.");
+            } else {
+                imageViews[currentRow][currentCol].setImageBitmap(BitmapFactory.decodeResource(getResources(), imageSrcs[currentRow][currentCol]));
+                //        go back one movement which will decrease number of movement, position as well
 
-            int previousRow = eb.myAllMovedPieces.get(movementsNum-2).x;
-            int previousCol = eb.myAllMovedPieces.get(movementsNum-2).y;
-
-            Log.d("MYINT", "Moving from current row " + currRow + " current col " + currCol);
-            Log.d("MYINT", "Moving to new row " + previousRow + " new col " + previousCol);
-//            its an issue with MODEL.
-//            as to check whether its possible to go back or not, we need to find out the specific previous coordinate
-//            but by doing that, its gonna go over the index of array
-//            e.g. I try to click go back once from starting point,
-//            in MODEL, it's checking the length of array inside its method,
-//            however, to get into this method, I need to put previous coordinate which is impossible.
-            if (eb.moveBackPreviousPieceSucceed(previousRow, previousCol)){
+                int previousRow = eb.myAllMovedPieces.get(movementsNum-2).x;
+                int previousCol = eb.myAllMovedPieces.get(movementsNum-2).y;
                 movementHappening(previousRow, previousCol);
                 //      update the number of movements as well
                 textViewForMovements.setText("Number of Movements : " + eb.countTotalMove());
-            } else {
-                warningMSG("You are at starting point, cannot go back");
             }
-
-
-
         } else {
             warningMSG("It only works when game is not finished");
         }
@@ -425,8 +487,8 @@ public class MainActivity extends AppCompatActivity {
 //            movementHappening();
 //
 ////        update the number of movements & Goal as well
-//            textViewForMovements.setText(R.string.number_of_movements + eyeball.getCurrentMoveCount());
-//            textViewForGoal.setText(R.string.number_of_goals + board.getGoals());
+//            textViewForMovements.setText("Number of Movements : " + eyeball.getCurrentMoveCount());
+//            textViewForGoal.setText("Number of Goal(s) : " + board.getGoals());
 //
 //        } else {
 //            warningMSG("You can only load the game when its not finished or been saved before");
